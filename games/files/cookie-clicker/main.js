badaaef1144e0a43dd6f08dfb73afc7e24576cad
tf7software <rhenrywarren@gmail.com>
2024-11-12 09:35:16 -0700

    Took away domain check
    For cookie clicker cuz I am a bad boy

--- games/files/cookie-clicker/main.js
+++ games/files/cookie-clicker/main.js
@@ -1104,11 +1104,13 @@ Game.Launch=function()
 	}
 	Game.ErrorFrame=function()
 	{
-		l('javascriptError').innerHTML=
-		'<div class="title">Oops. Wrong address!</div>'+
-		'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
-		'You can <a href="http://orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
-		'<small>(If for any reason, you are unable to access the game on the official URL, we are currently working on a second domain.)</small></div>';
+		// tf7software wuz here
+		// Commented out for LupineVault (github.com/lupinevault)
+		//l('javascriptError').innerHTML=
+		//'<div class="title">Oops. Wrong address!</div>'+
+		//'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
+		//'You can <a href="http://orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
+		//'<small>(If for any reason, you are unable to access the game on the official URL, we are currently working on a second domain.)</small></div>';
 	}
 	Game.timedout=false;
 	Game.Timeout=function()
