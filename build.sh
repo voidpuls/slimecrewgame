6f8e54ed52b8605d180412f1f8d40b3545f03849
tf7software <rhenrywarren@gmail.com>
2024-11-30 17:55:22 -0700

    thatsnotmynieghbr

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+curl -L -o games/files/buckshot-roulette/index.pck "https://buckshotroulette.online/steam/index.pck"
+
+curl -L -o games/files/thats-not-my-neighbor/index.pck "https://thatsnotmyneighbor.online/index.pck"
