fe5443556920ff8b29db72331052fe400befc7ec
tf7software <rhenrywarren@gmail.com>
2024-12-18 07:46:26 -0700

    buckshot sh*t

--- games/files/buckshot-roulette/index.service.worker.js
+++ games/files/buckshot-roulette/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734488736|94643570';
+const CACHE_VERSION = '1734533161|156949583';
 /** @type {string} */
 const CACHE_PREFIX = 'Buckshot Roulett-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
